da5a327fc6230efd40432345196fca48f750d269
2273653338@qq.com <2273653338@qq.com>
2020-10-24 20:27:24 +0800

    update third work

---
+++ build/precache-manifest.59dfdf86bdc8328d3f5ff108f31a927d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cba9842bf46b6931f3dadca6d492b52e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fdc12a565e54558cea3",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "4861a4e4d474174c15fb",
+    "url": "/static/css/main.06259c61.chunk.css"
+  },
+  {
+    "revision": "8fdc12a565e54558cea3",
+    "url": "/static/js/2.3ec34b9b.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.3ec34b9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4861a4e4d474174c15fb",
+    "url": "/static/js/main.53b3fe06.chunk.js"
+  },
+  {
+    "revision": "345b754ea4504c263d5b3e32280ff2ba",
+    "url": "/static/js/main.53b3fe06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
